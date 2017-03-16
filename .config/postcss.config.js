260f1aefebb6091d92cb53edf396162771e56bc0
in-in <0866222@gmail.com>
2017-03-17 00:33:06 +0300

    update postcss-cli and its config

---
+++ .config/postcss.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: [
+    require('postcss-easy-import')(),
+    require('postcss-custom-properties')(),
+    require('postcss-custom-media')(),
+    require('postcss-media-minmax')(),
+    require('postcss-assets')({
+      'basePath': 'src/'
+    }),
+    require('postcss-color-function')(),
+    require('postcss-pseudoelements')(),
+    require('autoprefixer')({
+      'browsers': 'last 10 versions'
+    }),
+    require('postcss-csso')()
+  ]
+}
